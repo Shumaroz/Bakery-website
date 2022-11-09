76ff66bb3cff07e96cb8f8eafdbbd0815b01caad
Gerlary <danan201@yandex.ru>
2022-11-09 11:07:20 +0300

    last pull Changes to be committed: 	modified:   cacke/__pycache__/views.cpython-36.pyc 	modified:   cacke/views.py

--- cacke/views.py
+++ cacke/views.py
@@ -13,19 +13,17 @@ def cacke_list (request):
             
             form = SearchForm(request.POST) 
             cakes = Bulka.objects.all()
-            request.POST
-            if form.is_valid():
-            #changed = form.changed_data
-                pric = form.get_price()
-                if pric[0] == "":
-                    pric[0] = 0
-                if pric[1] =="":
-                    pric[1] = 100000000000
-                pric[1]= str(int(pric[1])+1)
-                nam = form.get_name()
-                test = form.get_test()
-                stringe = form.get_nach()
-                cakes = cakes.filter(nachinka__icontains = stringe).filter(testo__icontains = test).filter(name__icontains = nam).exclude(price__gte = pric[1]).filter(price__gte=pric[0])
+       
+            pric = form.get_price()
+            if pric[0] == "":
+                pric[0] = 0
+            if pric[1] =="":
+                pric[1] = 100000000000
+            pric[1]= str(int(pric[1])+1)
+            nam = form.get_name()
+            test = form.get_test()
+            stringe = form.get_nach()
+            cakes = cakes.filter(nachinka__icontains = stringe).filter(testo__icontains = test).filter(name__icontains = nam).exclude(price__gte = pric[1]).filter(price__gte=pric[0])
             
          if(request.POST.get('Reset')):
             cakes=Bulka.objects.all()
